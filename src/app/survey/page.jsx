8028575e0e3bff55b599faffd216a60df94e6f42
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-29 17:14:58 +0500

    latest code

--- src/app/survey/page.jsx
+++ src/app/survey/page.jsx
@@ -23,6 +23,38 @@ export default function SurveyApp() {
                           <option value="by">by</option>
 
                       </select>
+                      <br />
+                      <label>Would you recommed to you friend ?</label>
+                      <input type="checkbox" />
+                      <label>Yes</label>
+                      <input type="checkbox" />
+                      <label>No</label>
+                      <input type="checkbox" />
+                      <label>Maybe</label>
+                      <label>Language and frameworks known (check all that apply)</label>
+                      <input type="checkbox" />
+                      <label>C</label>
+                      <input type="checkbox" />
+                      <label>C++</label>
+                      <input type="checkbox" />
+                      <label>C#</label>
+                      <input type="checkbox" />
+                      <label>Java</label>
+                      <input type="checkbox" />
+                      <label>Python</label>
+                      <input type="checkbox" />
+                      <label>Javascript</label>
+                      <input type="checkbox" />
+                      <label>React</label>
+                      <input type="checkbox" />
+                      <label>Angular</label>
+                      <input type="checkbox" />
+                      <label>Django</label>
+                      <input type="checkbox" />
+                      <label>Spring</label>
+                      <label>Any comments and suggestion</label>
+                      <textarea rows="4" cols="50" name="comment" form="usrform" />
+                      <button type="submit">Submit</button>
                   </div>
               </div>
           </div>
