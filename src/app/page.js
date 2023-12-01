b65527a42a3d16875c613b6efbfd0eecb49b44e6
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-12-01 17:04:32 +0500

    add some changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,56 +1,98 @@
-// import Image from 'next/image'
-// import styles from './page.module.css'
+import styles from "./page.module.css"
+export default function SurveyApp() {
+return (
+<>
+    <div className={styles.wrapper}>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1>Survey App</h1>
+            </div>
+                <div className={styles.formClass}>
+                     <div className={styles.inputfieldsClass}>
+                        <label>Name</label>
+                          <input type="text" />
+                          <label>Email</label>
+                          <input type="text" />
+                          <label>Age</label>
+                          <input type="text" />
+                          <label>Which option best describe you ?</label>
+                          <select>
+                              <option value=""></option>
+                              <option value="hi">hi</option>
+                              <option value="hello">hello</option>
+                              <option value="why">why</option>
+                              <option value="by">by</option>
+          
+                          </select>
+                    </div>
+                
+                <br />
+                <div className={styles.checkboxClass}>
+                    <label>Would you recommed to you friend ?</label>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>Yes</label>
 
-export default function Home() {
-  return (
-    //         <>
-//     <h2>Quaid-e-Azam, Mohammed Ali Jinnah</h2>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>No</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>Maybe</label>
+                    </div>
+                    <label>Language and frameworks known (check all that apply)</label>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>C</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>C++</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>C#</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>Java</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>Python</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>Javascript</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>React</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>Angular</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>Django</label>
+                    </div>
+                    <div className={styles.eachCheckbox}>
+                        <input type="checkbox" />
+                        <label>Spring</label>
+                    </div>
 
-//     <h3>Early Life and Education:</h3>
-//     <p>Mohammed Ali Jinnah, revered as Quaid-e-Azam (meaning "Great Leader"), was born on December 25, 1876, in Karachi,
-//         then part of British India. He belonged to a prosperous Gujarati family. His early education took place in
-//         Karachi, and he later moved to England for higher studies.
-//     </p>
-//     <h3>Legal Career:</h3>
-//     <p>Jinnah pursued law and became a successful barrister. He practiced law in Bombay, excelling in his field and
-//         earning a reputation for his exceptional skills as a lawyer.
-//     </p>
-//     <h3>Political Journey:</h3>
-//     <p>His political career began in the Indian National Congress, advocating for Hindu-Muslim unity. Over time, due to
-//         ideological differences and the Congress's limited support for Muslim rights, he grew disillusioned and later
-//         joined the All-India Muslim League in 1913.
-//     </p>
-//     <h3>Champion of Muslim Rights:</h3>
-//     <p>Jinnah emerged as a vocal advocate for the rights of Muslims in British India. He envisioned a separate nation
-//         for Muslims, advocating the "Two-Nation Theory," asserting that Muslims and Hindus were distinct nations with
-//         separate identities.
-//     </p>
-//     <h3> Leader of Pakistan Movement:</h3>
-//     <p>Quaid-e-Azam became the leader of the Pakistan Movement, tirelessly working towards the creation of a separate
-//         homeland for Muslims. His advocacy and strategic negotiations led to the eventual formation of Pakistan on
-//         August 14, 1947.
-//     </p>
-//     <h3>First Governor-General:</h3>
-//     <p>Upon Pakistan's independence, Jinnah became its first Governor-General, steering the newly formed nation through
-//         its initial challenges and laying the groundwork for its governance.
-//     </p>
-//     <h3>Legacy:</h3>
-//     <p>Quaid-e-Azam’s leadership and determination played a pivotal role in the creation of Pakistan. His commitment to
-//         democracy, equality, and religious freedom remains an integral part of Pakistan’s founding principles.
-//     </p>
-//     <h3>Demise:</h3>
-//     <p>Sadly, Jinnah’s health declined rapidly after Pakistan’s independence. He passed away on September 11, 1948,
-//         leaving behind a legacy of statesmanship, determination, and vision.
-//     </p>
-//     <h3>Conclusion:</h3>
-//     <p>Mohammed Ali Jinnah, Quaid-e-Azam, is remembered as the father of Pakistan, an emblematic leader whose unwavering
-//         resolve and vision transformed a dream into reality, providing a homeland for millions based on the principles
-//         of unity, faith, and discipline.
-//     </p>
-// </>
-    <main >
-      
-      
-    </main>
-  )
+                </div>
+                    <div className={styles.commentClass}>
+                      <label>Any comments and suggestion</label>
+                      <textarea rows="4" cols="50" name="comment" form="usrform" /> 
+                    </div>
+                <button type="submit">Submit</button>
+            </div>
+        </div>
+    </div>
+</>
+)
 }
